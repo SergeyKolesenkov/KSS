793e8af03c3943fb693c4e9182a7051de5436fb2
SergeyKolesenkov <kolesenkovs@bk.ru>
2026-05-09 23:41:50 +0300

    Homework_20

--- mysite/settings.py
+++ mysite/settings.py
@@ -172,9 +172,12 @@ LANGUAGES = [
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'uploads'
